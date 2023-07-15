2b2c6c84491738f11ad81d60b41097cedc66308a
ZIshmael <38586415+Zaederx@users.noreply.github.com>
2023-07-15 13:50:02 +0200

    update simplytest.ts

--- js/simplytest.js
+++ js/simplytest.js
@@ -1,33 +1,26 @@
 import { compareTwoStrings } from "./simplystring.js";
+import { testOuput } from "./test-output.js";
+var test = 1;
 var str1 = 'string';
 var str2 = 'string'; //exactly the same
-var comparisionScore1 = compareTwoStrings(str1, str2);
-console.log(`comparisionScore1: ${comparisionScore1}`); //prints 1 // completely the same
-var expectedAns1 = 1;
-if (comparisionScore1 == expectedAns1) {
-    console.log('test 1 passed');
-}
+const expectedAns1 = 1;
+const comparisionScore1 = compareTwoStrings(str1, str2);
+testOuput(test, str1, str2, expectedAns1, comparisionScore1);
+var test = 2;
 var str1 = 'string';
 var str2 = 'trngsi'; //'string' jumbled up
-var comparisionScore2 = compareTwoStrings(str1, str2);
-console.log(`comparisionScore2: ${comparisionScore2}`); //prints '0.7555555555555555' //can tell that it's very similar but not quite the same
-var expectedAns2 = 0.7555555555555555;
-if (comparisionScore2 == expectedAns2) {
-    console.log('test 2 passed');
-}
+const expectedAns2 = 0.7555555555555555;
+const comparisionScore2 = compareTwoStrings(str1, str2);
+testOuput(test, str1, str2, expectedAns2, comparisionScore2); //prints '0.7555555555555555' //can tell that it's very similar but not quite the same
+var test = 3;
 var str1 = 'string';
 var str2 = 'tr8390'; //only two characters in common
-var comparisionScore3 = compareTwoStrings(str1, str2);
-console.log(`comparisionScore3: ${comparisionScore3}`); //prints 0.3333333333333333 //not very similar
 const expectedAns3 = 0.3333333333333333;
-if (comparisionScore3 == expectedAns3) {
-    console.log('test 3 passed');
-}
+const comparisionScore3 = compareTwoStrings(str1, str2);
+testOuput(test, str1, str2, expectedAns3, comparisionScore3); //prints 0.3333333333333333 //not very similar
+var test = 4;
 var str1 = 'string';
 var str2 = '00'; //no characters in common
-var comparisionScore4 = compareTwoStrings(str1, str2);
-console.log(`comparisionScore4: ${comparisionScore4}`); //prints 0 //not at all the same
 var expectedAns4 = 0;
-if (comparisionScore4 == expectedAns4) {
-    console.log('test 4 passed');
-}
+var comparisionScore4 = compareTwoStrings(str1, str2);
+testOuput(test, str1, str2, expectedAns4, comparisionScore4); //prints 0 //not at all the same
